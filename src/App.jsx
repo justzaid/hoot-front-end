1bd9845c41b0a38c3d25dd874ef26a0fddea6f6a
justzxid <zaidmk96@gmail.com>
2025-02-27 15:07:48 +0300

    Front end structure completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,62 @@
-import { useState, createContext } from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { useState, createContext, useEffect } from 'react';
+import { Routes, Route, useNavigate } from 'react-router-dom';
+
+// Components
 import NavBar from './components/NavBar/NavBar';
 import Landing from './components/Landing/Landing';
 import Dashboard from './components/Dashboard/Dashboard';
 import SignupForm from './components/SignupForm/SignupForm';
 import SigninForm from './components/SigninForm/SigninForm';
-import * as authService from '../src/services/authService'; // import the authservice
+import HootList from './components/HootList/HootList';
+import HootDetails from './components/HootDetails/HootDetails';
+import HootForm from './components/HootForm/HootForm';
+
+// Services
+import * as authService from '../src/services/authService';
+import * as hootService from './services/hootService';
 
 export const AuthedUserContext = createContext(null);
 
 const App = () => {
-  const [user, setUser] = useState(authService.getUser()); // using the method from authservice
+  const [user, setUser] = useState(authService.getUser());
+  const [hoots, setHoots] = useState([])
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const fetchAllHoots = async () => {
+      const hootsData = await hootService.index();
+      setHoots(hootsData)
+    }
+  if(user) fetchAllHoots()
+  }, [user])
 
   const handleSignout = () => {
     authService.signout();
     setUser(null);
+  };
+
+  const handleAddHoot = async (hootFormData) => {
+    const newHoot = await hootService.create(hootFormData);
+    const newHootList = [newHoot, ...hoots]
+    setHoots(newHootList);
+    navigate('/hoots');
+  };
+  
+  const handleUpdateHoot = async (hootId, hootFormData) => {
+    const updatedHoot = await hootService.update(hootId, hootFormData);
+
+    setHoots(hoots.map((hoot) => (hootId === hoot._id ? updatedHoot : hoot)));
+
+    navigate(`/hoots/${hootId}`);
+  };
+
+  const handleDeleteHoot = async (hootId) => {
+    // Call upon the service function:
+    const deletedHoot = await hootService.deleteHoot(hootId);
+    // Filter state using deletedHoot._id:
+    setHoots(hoots.filter((hoot) => hoot._id !== deletedHoot._id));
+    // Redirect the user:
+    navigate('/hoots');
   };
 
   return (
@@ -23,8 +65,17 @@ const App = () => {
         <NavBar user={user} handleSignout={handleSignout} />
         <Routes>
           {user ? (
+            // protected
+            <>
             <Route path="/" element={<Dashboard user={user} />} />
+            <Route path="/hoots" element={<HootList hoots={hoots}/>} />
+            <Route path="/hoots/:hootId" element={<HootDetails handleDeleteHoot={handleDeleteHoot}/>} />
+            <Route path="/hoots/new" element={<HootForm handleAddHoot={handleAddHoot} />} />
+            <Route path="/hoots/:hootId/edit" element={<HootForm handleUpdateHoot={handleUpdateHoot} />}
+/>
+            </>
           ) : (
+            // Public
             <Route path="/" element={<Landing />} />
           )}
           <Route path="/signup" element={<SignupForm setUser={setUser} />} />
